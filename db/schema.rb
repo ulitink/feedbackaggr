eba0a5871e54d2c59141cadead7f0a2e4119b9cd
Konstantin Ulitin <ulitin.k@gmail.com>
2011-10-22 23:08:19 +0400

    Basic auth

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111022150246) do
+ActiveRecord::Schema.define(:version => 20111022172326) do
 
   create_table "feeds", :force => true do |t|
     t.string   "loader"
@@ -33,8 +33,15 @@ ActiveRecord::Schema.define(:version => 20111022150246) do
   end
 
   create_table "users", :force => true do |t|
+    t.string   "email",                                 :default => "", :null => false
+    t.string   "encrypted_password",     :limit => 128, :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
